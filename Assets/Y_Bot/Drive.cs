f7eeb14f318c9a7866807cc43ae436617f1f8b2a
Pål Grønstad Solheim <paul.solheim@gmail.com>
2019-03-21 13:07:51 +0100

    Etter 1. Gang
    Etter 1. kodekveld med YBot.

---
+++ Assets/Y_Bot/Drive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    float speed = 5.0F;
+    float rotationSpeed = 100.0F;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0,0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if(translation > 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
